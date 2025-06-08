58fd66bb6ed904e06c51a135e8d460d4a04dc206
Ayman <168225570+aymnsk@users.noreply.github.com>
2025-06-08 12:55:38 +0530

    Update deepdream.py

--- deepdream.py
+++ deepdream.py
@@ -1,1 +1,9 @@
-# def apply_deepdream(image): return image
+from PIL import Image, ImageEnhance, ImageFilter
+
+def apply_deepdream(image):
+    image = image.resize((512, 512))
+    image = image.filter(ImageFilter.DETAIL)
+    image = image.filter(ImageFilter.EDGE_ENHANCE)
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(1.5)
+    return image
