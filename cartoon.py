fc91d390b4e39306afd0792956015714498995fe
Ayman <168225570+aymnsk@users.noreply.github.com>
2025-06-08 12:56:00 +0530

    Update cartoon.py

--- cartoon.py
+++ cartoon.py
@@ -1,2 +1,23 @@
-# def cartoonize(image): return image
-# def pencil_sketch(image): return image
+import cv2
+import numpy as np
+from PIL import Image
+
+def cartoonize(pil_img):
+    img = np.array(pil_img)
+    img = cv2.resize(img, (512, 512))
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    blur = cv2.medianBlur(gray, 7)
+    edges = cv2.adaptiveThreshold(blur, 255,
+                                  cv2.ADAPTIVE_THRESH_MEAN_C,
+                                  cv2.THRESH_BINARY, 9, 9)
+    color = cv2.bilateralFilter(img, 9, 250, 250)
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    return Image.fromarray(cartoon)
+
+def pencil_sketch(pil_img):
+    img = np.array(pil_img)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    inv = 255 - gray
+    blur = cv2.GaussianBlur(inv, (21, 21), 0)
+    sketch = cv2.divide(gray, 255 - blur, scale=256)
+    return Image.fromarray(sketch)
