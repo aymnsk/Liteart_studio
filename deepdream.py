aa382bd5a2a0039d9a9387b9d24d0cf9c40b803f
Ayman <168225570+aymnsk@users.noreply.github.com>
2025-06-08 13:16:42 +0530

    Update deepdream.py

--- deepdream.py
+++ deepdream.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageFilter, ImageEnhance
 
 def apply_deepdream(img: Image.Image) -> Image.Image:
-    # Simple fake "deepdream" effect by enhancing edges and saturation
+    # Simple fake "deepdream" effect by enhancing edges and color
     img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
     enhancer = ImageEnhance.Color(img)
     img = enhancer.enhance(1.8)
