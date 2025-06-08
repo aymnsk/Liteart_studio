54d887f3b9039f87190d88d3a4512e4bae98861e
Ayman <168225570+aymnsk@users.noreply.github.com>
2025-06-08 16:21:41 +0530

    Update fst.py

--- fst.py
+++ fst.py
@@ -19,24 +19,23 @@ class ConvLayer(nn.Module):
 class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
-        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
-        self.in4 = nn.InstanceNorm2d(channels, affine=True, track_running_stats=True)
-        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
-        self.in5 = nn.InstanceNorm2d(channels, affine=True, track_running_stats=True)
+        self.conv1 = ConvLayer(channels, channels, 3, 1)
+        self.in1 = nn.InstanceNorm2d(channels, affine=True)
         self.relu = nn.ReLU()
+        self.conv2 = ConvLayer(channels, channels, 3, 1)
+        self.in2 = nn.InstanceNorm2d(channels, affine=True)
 
     def forward(self, x):
         residual = x
-        out = self.relu(self.in4(self.conv1(x)))
-        out = self.in5(self.conv2(out))
+        out = self.relu(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
         return out + residual
 
 class UpsampleConvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
         super().__init__()
         self.upsample = upsample
-        padding = kernel_size // 2
-        self.reflection_pad = nn.ReflectionPad2d(padding)
+        self.reflection_pad = nn.ReflectionPad2d(kernel_size // 2)
         self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride)
 
     def forward(self, x):
@@ -47,23 +46,27 @@ class UpsampleConvLayer(nn.Module):
 class TransformerNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
-        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
-        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32, 9, 1)
+        self.in1 = nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, 3, 2)
+        self.in2 = nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, 3, 2)
+        self.in3 = nn.InstanceNorm2d(128, affine=True)
 
-        self.in1 = nn.InstanceNorm2d(32, affine=True, track_running_stats=True)
-        self.in2 = nn.InstanceNorm2d(64, affine=True, track_running_stats=True)
-        self.in3 = nn.InstanceNorm2d(128, affine=True, track_running_stats=True)
-
+        # Residual layers
         self.res1 = ResidualBlock(128)
         self.res2 = ResidualBlock(128)
         self.res3 = ResidualBlock(128)
         self.res4 = ResidualBlock(128)
         self.res5 = ResidualBlock(128)
 
-        self.deconv1 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2)
-        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2)
-        self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(128, 64, 3, 1, upsample=2)
+        self.in4 = nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = UpsampleConvLayer(64, 32, 3, 1, upsample=2)
+        self.in5 = nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, 9, 1)
 
         self.relu = nn.ReLU()
 
@@ -76,58 +79,36 @@ class TransformerNet(nn.Module):
         y = self.res3(y)
         y = self.res4(y)
         y = self.res5(y)
-        y = self.relu(self.deconv1(y))
-        y = self.relu(self.deconv2(y))
+        y = self.relu(self.in4(self.deconv1(y)))
+        y = self.relu(self.in5(self.deconv2(y)))
         y = self.deconv3(y)
         return y
 
-def remap_keys(state_dict):
-    # Fix flat "in4", "in5", ..., "in13" keys by assigning them to res1, ..., res5 blocks
-    mapping = {}
-    for key in list(state_dict.keys()):
-        if key.startswith("in"):
-            num = int(key[2:].split(".")[0])
-            if 4 <= num <= 13:
-                block_idx = (num - 4) // 2 + 1
-                new_key = f"res{block_idx}.in{num}" + key[len(f'in{num}'):]
-                mapping[key] = new_key
-    for old_k, new_k in mapping.items():
-        state_dict[new_k] = state_dict.pop(old_k)
-    return state_dict
-
 def load_model(style_name):
     model_path = os.path.join("models", f"{style_name}.pth")
-    if not os.path.exists(model_path):
-        raise FileNotFoundError(f"Model not found: {model_path}")
-    
     model = TransformerNet()
     state_dict = torch.load(model_path, map_location=device)
-    state_dict = remap_keys(state_dict)
-    model.load_state_dict(state_dict, strict=True)
+    model.load_state_dict(state_dict)
     model.to(device).eval()
     return model
 
 def apply_style_transfer(content_img, style_name="mosaic"):
-    preprocess = transforms.Compose([
+    transform = transforms.Compose([
         transforms.Resize(512),
+        transforms.CenterCrop(512),
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: x[:3]),  # keep RGB only
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    post_transform = transforms.Compose([
+        transforms.Lambda(lambda x: x.clamp(0, 255)),
+        transforms.Lambda(lambda x: x.div(255)),
+        transforms.ToPILImage()
     ])
 
-    postprocess = transforms.Compose([
-        transforms.Normalize(mean=[-2.118, -2.036, -1.804],
-                             std=[4.367, 4.464, 4.444]),
-        transforms.Lambda(lambda x: torch.clamp(x, 0, 1)),
-        transforms.ToPILImage(),
-    ])
+    content_tensor = transform(content_img).unsqueeze(0).to(device)
 
     model = load_model(style_name)
-
-    content_tensor = preprocess(content_img).unsqueeze(0).to(device)
     with torch.no_grad():
         output_tensor = model(content_tensor).cpu().squeeze(0)
-    output_img = postprocess(output_tensor)
 
-    return output_img
+    return post_transform(output_tensor)
